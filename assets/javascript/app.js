60b85b59671e1082062cbbab36c4aa17def6ce3c
Lorena Ontiveros <lorena0328o@gmail.com>
2019-07-16 20:46:09 -0500

    deleted everything (broke my heart) psuedo coded new idea

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+//grab zip code from user submit area to apply to weather api
+//wheather api
+//document ready
+//apply api and queryURL
+//performing an ajax get request to our queryurl
+//display weather on left side of page
+
+
+//cocktail api
+//apply api and queryURL
+//performing an ajax get request to our queryurl
+//display cocktails on right side
+//pull weather and tell computer
+//display drinks according to weather
